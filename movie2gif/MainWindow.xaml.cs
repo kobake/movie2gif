e06055af7d18ed148445c5eb48a80c7d538fd8d8
kobake <kobake@users.sourceforge.net>
2016-12-10 13:27:19 +0900

    ファイル名入力対応

--- movie2gif/MainWindow.xaml.cs
+++ movie2gif/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +14,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace movie2gif
 {
@@ -23,6 +25,45 @@ namespace movie2gif
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Log(string text)
+        {
+            LogText.Text += text + "\n";
+            LogText.CaretIndex = LogText.Text.Length;
+            LogText.ScrollToEnd();
+        }
+
+        private void InputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "MovieFile(*.avi;*.mp4;*.mpg;*.mov;*.flv;*.wmv)|*.avi;*.mp4;*.mpg;*.mov;*.flv;*.wmv|AnyFile(*.*)|*.*";
+            dialog.Title = "Select source movie file";
+            dialog.RestoreDirectory = true;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            bool? ret = dialog.ShowDialog();
+            if(ret == true)
+            {
+                InputFilePath.Text = dialog.FileName;
+                OutputFilePath.Text = Regex.Replace(dialog.FileName, @"\.[A-Za-z0-9_]+$", ".gif");
+            }
+        }
+
+        private void OutputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.InitialDirectory = Path.GetDirectoryName(OutputFilePath.Text);
+            dialog.Filter = "GitFile(*.gif)|*.gif";
+            dialog.Title = "Select destination gif file";
+            dialog.RestoreDirectory = true;
+            dialog.CheckFileExists = false;
+            dialog.CheckPathExists = true;
+            bool? ret = dialog.ShowDialog();
+            if (ret == true)
+            {
+                OutputFilePath.Text = dialog.FileName;
+            }
         }
     }
 }
